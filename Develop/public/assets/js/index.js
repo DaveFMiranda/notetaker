2f670ce091dc5cd4dc280f607214076dc483f06a
Noob Evad <davefrankmiranda@gmail.com>
2023-05-28 19:07:53 -0500

    initial commit
    clear
    cd.
    cd ..

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -4,6 +4,23 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
+/* Stuff to do somewhere in here:
+1) Start by creating routes? create notes to be imported? No idea
+
+The following HTML routes should be created:
+• GET /notes should return the notes.html file.
+• GET * should return the index.html file.
+The following API routes should be created:
+• GET /api/notes should read the db.json file and return all saved notes as JSON.
+• POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+ALSO:
+• Application front end must connect to an Express.js back end.
+• Application back end must store notes with unique IDs in a JSON file.
+• Application must be deployed to Heroku.
+
+*/
+
+
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
